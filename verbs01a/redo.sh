eef632b3d575415f922fb1d6ded2d789cc2a929e
funderburkjim <funderburkjim@gmail.com>
2020-07-07 15:21:31 -0400

    verbs01a: use sanskrit-lexicon/MWS/mwverbs/mwverbs1.txt

--- verbs01a/redo.sh
+++ verbs01a/redo.sh
@@ -1,10 +1,12 @@
-echo "remake mwverbs"
-python mwverb.py mw ../../mw/mw.txt mwverbs.txt
-echo "remake mwverbs1"
-python mwverbs1.py mwverbs.txt mwverbs1.txt
+#echo "remake mwverbs"
+#python mwverb.py mw ../../mw/mw.txt mwverbs.txt
+#echo "remake mwverbs1"
+#python mwverbs1.py mwverbs.txt mwverbs1.txt
+orig="../../../cologne/csl-orig/v02"
+mwverbs1="../../MWS/mwverbs/mwverbs1.txt"
 echo "remake pwg_verb_filter.txt"
-python pwg_verb_filter.py ../pwg.txt pwg_verb_exclude.txt pwg_verb_include.txt pwg_verb_filter.txt
+python pwg_verb_filter.py ${orig}/pwg/pwg.txt pwg_verb_exclude.txt pwg_verb_include.txt pwg_verb_filter.txt
 echo "remake pwg_verb_filter_map.txt"
-python pwg_verb_filter_map.py pwg_verb_filter.txt pw_mw_map_edit.txt mwverbs1.txt pwg_verb_filter_map.txt
+python pwg_verb_filter_map.py pwg_verb_filter.txt pw_mw_map_edit.txt ${mwverbs1} pwg_verb_filter_map.txt
 echo "remake pwg_preverb1.txt"
-python preverb1.py slp1 ../pwg.txt pwg_verb_filter_map.txt mwverbs1.txt pwg_preverb1.txt
+python preverb1.py slp1 ../pwg.txt pwg_verb_filter_map.txt ${mwverbs1} pwg_preverb1.txt
